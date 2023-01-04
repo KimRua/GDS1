700dc7a82dddda4612379eb5e06b99e678efb646
KimRua <rua.kim.04@gmail.com>
2023-01-05 04:12:03 +0900

    trig start

--- GDS1.py
+++ GDS1.py
@@ -1,5 +1,4 @@
 import random, math, sys
-from fractions import Fraction
 
 # 교과목 선택 함수
 def select_subject():
@@ -9,17 +8,14 @@ def select_subject():
     return answer
 
 # 입력 오류 확인 함수 (start~end사이의 정상 값이면 값을, 오류가 나거나 값을 벗어나면 -1을 리턴)
-def input_filter(answer, start=0, end=0, isStr=False, isExpression=False):
+def input_filter(answer, start=0, end=0, isStr=False):
     if answer == 'help' or answer == '도움': return help()
-    if isStr: return answer.replace(' ', '')
-    try: 
-        if isExpression:
-            a, b = map(int, answer.split('/'))
-            return round(a/b, 4)
-        answer = eval(answer)
+    answer = answer.replace(' ', '')
+    if isStr: return answer
+    try: answer = eval(answer.replace('^', '**'))
     except: return -1
     if isinstance(answer, tuple): return answer
-    if start <= answer <= end or start==end: return answer
+    if start <= answer <= end or start==end: return round(answer, 4)
     else: return -1
 
 # 정답 확인 함수
@@ -41,16 +37,13 @@ def exp():
     print("\n[지수]\n- 문제유형 1, 2, 3, 4 총 4가지가 있습니다.\n")
     answer = -1
     while answer == -1:
-        answer = input_filter(input("\n[지수]\n- 학습하고자하는 문제 유형을 입력해주세요.\n거듭제곱 : 1\n지수법칙 : 2\n거듭제곱근(실수) : 3\n지수가 실수인 경우 : 4\n입력 : "), start=0, end=4)
+        answer = input_filter(input("\n[지수]\n- 학습하고자하는 문제 유형을 입력해주세요.\n1 : 거듭제곱\n2 : 지수법칙\n3 : 거듭제곱근(실수)\n4 : 지수가 실수인 경우\n입력 : "), start=0, end=4)
     if answer != 0: # 0이라면 바로 종료
         count = 0
         for i in range(1, 6): # 5문제 출제
             count += exp_problem(i, answer)
         print(f'\n[결과]\n- {count}/5 문제 맞음. {count*20}점')
-
-
-# 문제 출제 함수 (랜덤, 매스, 그래프 등)
-# TODO: 마지막 문제 만들기
+# 지수 문제 출제 함수
 def exp_problem(index, category):
     print(f'\n[문제{index}]')
     result = 0
@@ -87,25 +80,27 @@ def exp_problem(index, category):
         result = (round(abs(a) ** (1/b)), round(-abs(a) ** (1/b))) if b % 2 == 0 else (round(abs(a) ** (1/b) if a > 0 else round(-abs(a)**(1/b))))
         print(f'{a}의 {b}제곱근을 구하시오.')
     elif category == 4: # 지수가 실수인 경우
-        result = 0
-        pass
-
+        num1 = random.choice([1,2,3,4,5,6,7,8,9,10,16,25,27,36,64])
+        num2 = random.randint(1, 5)
+        num3 = random.randint(1, 5)
+        print(f'{num1} ^ {num2}/{num3}')
+        result = round(num1 ** (num2/num3), 4)
+    
     answer = input_filter(input('정답을 입력해주세요. : '), isStr=True if category==2 else False)
     return result_check(result, answer)
 
 # 로그 교과목
 def log():
-    print("\n[로그]\n- 문제유형 1, 2, 3, 4 총 4가지가 있습니다.\n")
+    print("\n[로그]\n- 문제유형 1, 2 총 2가지가 있습니다.\n")
     answer = -1
     while answer == -1:
-        answer = input_filter(input("\n[로그]\n- 학습하고자하는 문제 유형을 입력해주세요.\n로그 값 구하기 : 1\n로그의 밑의 변환 : 2\n상용로그 : 3\n입력 : "), start=0, end=3)
+        answer = input_filter(input("\n[로그]\n- 학습하고자하는 문제 유형을 입력해주세요.\n1 : 로그 값 구하기\n2 : 로그의 밑의 변환\n입력 : "), start=0, end=2)
     if answer != 0: # 0이라면 바로 종료
         count = 0
         for i in range(1, 6): # 5문제 출제
             count += log_problem(i, answer)
         print(f'\n[결과]\n- {count}/5 문제 맞음. {count*20}점')
-
-# TODO: 3문제 만들기   
+# 로그 문제 출제 함수
 def log_problem(index, category):
     print(f'\n[문제{index}]')
     result = 0
@@ -127,7 +122,6 @@ def log_problem(index, category):
             num1 = 1/num1
             num2 = 1/num2
         result = round(math.log(num2, num1))
-        answer = input_filter(input('정답을 입력해주세요. : '))
     elif category == 2: # 로그의 밑의 변환
         problem_list = [
             [8, 16], [9, 27],
@@ -138,14 +132,49 @@ def log_problem(index, category):
         num1, num2 = random.choice(problem_list)
         print(f'log_{num1} {num2}')
         a = round(math.log(num2, num1), 4)
-        result = a
-        answer = input_filter(input('정답을 입력해주세요. : '), isExpression=True)
-    elif category == 3: # 상용로그
-        pass
+        result = a  
+    answer = input_filter(input('정답을 입력해주세요. : '))
     return result_check(result, answer)
 
 def trig():
-    return 0
+    print("\n[삼각함수]\n- 문제유형 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 총 10가지가 있습니다.\n")
+    answer = -1
+    while answer == -1:
+        answer = input_filter(input("\n[삼각함수]\n- 학습하고자하는 문제 유형을 입력해주세요.\n1 : 동경이 나타내는 일반각\n2 : 제n사분면\n3 : 부채꼴의 호의 길이와 넓이\n4 : 삼각함수\n5 : 사인함수 그래프 성질\n6 : 코사인함수 그래프 성질\n7 : 탄젠트함수 그래프 성질\n8 : 사인법칙\n9 : 코사인법칙\n10 : 삼각형의 넓이\n입력 : "), start=0, end=10)
+    if answer != 0: # 0이라면 바로 종료
+        count = 0
+        for i in range(1, 6): # 5문제 출제
+            count += trig_problem(i, answer)
+        print(f'\n[결과]\n- {count}/5 문제 맞음. {count*20}점')
+
+# TODO: 문제 1~10번 작성하기
+def trig_problem(index, category):
+    print(f'\n[문제{index}]')
+    result = 0
+    if category == 1: # 동경이 나타내는 일반각
+        pass
+    elif category == 2: # 제n사분면
+        pass
+    elif category == 3: # 부채꼴의 호의 길이와 넓이
+        pass
+    elif category == 4: # 삼각함수
+        pass
+    elif category == 5: # 사인함수 그래프 성질
+        pass
+    elif category == 6: # 코사인함수 그래프 성질
+        pass
+    elif category == 7: # 탄젠트함수 그래프 성질
+        pass
+    elif category == 8: # 사인법칙
+        pass
+    elif category == 9: # 코사인법칙
+        pass
+    elif category == 10: # 삼각형의 넓이
+        pass
+    
+    answer = input_filter(input('정답을 입력해주세요. : '))
+    return result_check(result, answer)
+
 def seq():
     return 0
 
